382096b3bd96acaf0295221768150eb8f491af6b
Sason Braha <sasonbraha@gmail.com>
2020-02-28 14:43:03 +0200

    Added bundle analyzer

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const { when } = require('@craco/craco');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+	webpack: {
+		plugins: [
+			...when(
+				Boolean(process.env.ANALYZE),
+				() => [new BundleAnalyzerPlugin()],
+				[]
+			)
+		]
+	}
+};
