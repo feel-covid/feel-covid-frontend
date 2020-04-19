3b87d3024fba6172f782004fcebc9ed0baf350f8
Sason Braha <sasonbraha@gmail.com>
2020-04-19 11:11:02 +0300

    Fixed infinite

--- src/utils/getStats.ts
+++ src/utils/getStats.ts
@@ -1,5 +1,10 @@
 export const getStats = (before: number, current: number) => {
-	const percentDiff = (Math.abs((before - current) / before) * 100).toFixed(1);
+	let percentDiff = (Math.abs((before - current) / before) * 100).toFixed(1);
+
+	if (Number(percentDiff) === Number.POSITIVE_INFINITY) {
+		percentDiff = (100).toFixed(1);
+	}
+
 	const actualDiff = current - before;
 	const trend = actualDiff === 0 ? 0 : Math.sign(actualDiff);
 
